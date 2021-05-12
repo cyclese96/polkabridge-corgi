13fd53b01b7c549683e6316af0857dbbc81bc4ec
Aamir Alam <aamiralam1991@gmail.com>
2021-05-12 21:46:23 +0530

    Add About page, Fix Header responsive

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,13 +1,31 @@
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { Receipt } from '@material-ui/icons';
-import { Fade } from 'react-reveal';
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Receipt } from "@material-ui/icons";
+import { Fade } from "react-reveal";
+import CustomButton from "../../common/CustomButton";
 
 const useStyles = makeStyles((theme) => ({
   background: {
     padding: 80,
-    height: '100%',
-    [theme.breakpoints.down('sm')]: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      padding: 10,
+      paddingBottom: 35,
+      paddingTop: 35,
+    },
+  },
+  background2: {
+    padding: 80,
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column-reverse",
       padding: 10,
       paddingBottom: 35,
       paddingTop: 35,
@@ -15,24 +33,25 @@ const useStyles = makeStyles((theme) => ({
   },
   heading: {
     color: theme.palette.pbr.textSecondary,
-    textAlign: 'left',
+    // textAlign: "center",
     fontSize: 36,
     fontWeight: 600,
-    verticalAlign: 'middle',
-    wordSpacing: '0px',
-    [theme.breakpoints.down('sm')]: {
+    verticalAlign: "middle",
+    wordSpacing: "0px",
+    paddingBottom: 5,
+    [theme.breakpoints.down("sm")]: {
       fontSize: 28,
     },
   },
   para: {
     fontWeight: 400,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
     paddingTop: 10,
-    paddingBottom: 10,
-    textAlign: 'left',
-    [theme.breakpoints.down('sm')]: {
+    paddingBottom: 40,
+    // textAlign: "center",
+    [theme.breakpoints.down("sm")]: {
       fontSize: 16,
     },
   },
@@ -41,101 +60,180 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.pbr.primary,
   },
   button: {
-    borderRadius: '50px',
+    borderRadius: "50px",
     background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    lineHeight: '24px',
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    lineHeight: "24px",
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
     marginTop: 15,
-    color: '#ffffff',
-    padding: '15px 30px 15px 30px',
+    color: "#ffffff",
+    padding: "15px 30px 15px 30px",
     fontWeight: 600,
     fontSize: 14,
   },
   icon: {
     fontSize: 16,
     marginRight: 7,
-    color: '#ffffff',
+    color: "#ffffff",
   },
   title: {
     fontWeight: 500,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 22,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       fontSize: 20,
     },
   },
   imageContainer: {
-    width: '100%',
-    [theme.breakpoints.down('sm')]: {
+    width: 500,
+    height: "auto",
+    marginLeft: 10,
+    marginRight: 10,
+    [theme.breakpoints.down("sm")]: {
       marginTop: 30,
-      height: '100%',
+      height: "100%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: 300,
+      height: "auto",
     },
   },
   focusContainer: {
-    border: '1px solid #888888',
+    border: "1px solid #888888",
     padding: 10,
-    width: '100%',
-    borderRadius: '7px',
+    width: "100%",
+    borderRadius: "7px",
     marginRight: 10,
     marginBottom: 10,
     height: 110,
-    [theme.breakpoints.down('sm')]: {
-      height: 'auto',
+    [theme.breakpoints.down("sm")]: {
+      height: "auto",
     },
+  },
+  textContainer: {
+    maxWidth: 500,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    marginLeft: 10,
+    marginRight: 10,
+    [theme.breakpoints.down("xs")]: {
+      marginTop: 100,
+      marginLeft: 30,
+      // alignItems: "center",
+    },
+  },
+  number: {
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    backgroundColor: theme.palette.pbr.primary,
+    color: "#f9f9f9",
+    textAlign: "center",
+    verticalAlign: "baseline",
+    paddingTop: 5,
+    marginBottom: 20,
   },
 }));
 export default function About() {
   const classes = useStyles();
 
   return (
-    <div className={classes.background}>
-      <div className="row g-0 mt-3">
-        <div className="col-md-6">
-          <h6 className={classes.heading}>
-            What is <strong className={classes.highlight}>CORGI?</strong>
-          </h6>
-          <p className={classes.para}>We are a part of the newly emerging Tiger family!</p>
+    <>
+      <section>
+        <div className={classes.background}>
+          <div className={classes.textContainer}>
+            <Fade left>
+              <h3 className={classes.number}>1</h3>
+              <h6 className={classes.heading}>
+                What is <strong className={classes.highlight}>CORGI?</strong>
+              </h6>
+              <p className={classes.para}>
+                We are a part of the newly emerging Tiger family!
+              </p>
 
-          <Fade left>
-            {/* <div className="d-flex justify-content-center mt-3">
-              <div className={classes.focusContainer}>
-                <h6 className={classes.title}>Decentralized</h6>
-                <p className={classes.para}>Fully control your own crypto. Tokens trade wallet-to-wallet.</p>
-              </div>
-              <div className={classes.focusContainer}>
-                <h6 className={classes.title}>Secure</h6>
-                <p className={classes.para}>Your funds are secured by an open-source smart contract.</p>
-              </div>
+              <CustomButton
+                className={classes.button}
+                title={"Find out more"}
+              ></CustomButton>
+            </Fade>
+          </div>
+
+          <Fade right>
+            <div>
+              <img
+                src="https://sanshuinu.finance/assets/images/sanshu-5.png"
+                alt="explain"
+                className={classes.imageContainer}
+              />
             </div>
-            <div className="d-flex justify-content-center">
-              <div className={classes.focusContainer}>
-                <h6 className={classes.title}>High performance</h6>
-                <p className={classes.para}>UI is simple and fast.</p>
-              </div>
-              <div className={classes.focusContainer}>
-                <h6 className={classes.title}>Fully Audited</h6>
-                <p className={classes.para}>Audited by Certik.</p>
-                <a href="https://certik.org/projects/polkabridge">Check report</a>
-              </div>
-            </div>
-            <div className="text-left mt-3">
-              <Button
-                href="https://www.youtube.com/watch?v=tsbRzb2S7mU"
-                target="_blank"
-                variant="contained"
-                color="primary"
-                className={classes.button}>
-                <Receipt className={classes.icon} /> Learn More
-              </Button>
-            </div> */}
           </Fade>
         </div>
-        <div className="col-md-6">
+      </section>
+
+      <section>
+        <div className={classes.background2}>
+          <Fade right>
+            <div>
+              <img
+                src="https://sanshuinu.finance/assets/images/sanshu-4.png"
+                alt="explain"
+                className={classes.imageContainer}
+              />
+            </div>
+          </Fade>
+
+          <div className={classes.textContainer}>
+            <Fade left>
+              <h3 className={classes.number}>2</h3>
+              <h6 className={classes.heading}>
+                A fully{" "}
+                <strong className={classes.highlight}>
+                  deflationary token
+                </strong>{" "}
+                that appreciates every single transaction
+              </h6>
+              <p className={classes.para}>
+                We are a part of the newly emerging Tiger family!
+              </p>
+
+              <CustomButton
+                className={classes.button}
+                title={"See all features"}
+              ></CustomButton>
+            </Fade>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className={classes.background}>
+          <div className={classes.textContainer}>
+            <Fade left>
+              <h3 className={classes.number}>3</h3>
+              <h6 className={classes.heading}>
+                Why choose us?{" "}
+                <strong className={classes.highlight}>CORGI?</strong> is fully
+                decentralized, had a fair launch and an exciting roadmap.
+              </h6>
+              {/* <p className={classes.para}>
+                We are a part of the newly emerging Tiger family!
+              </p> */}
+
+              <CustomButton
+                className={classes.button}
+                title={"View our roadmap"}
+              ></CustomButton>
+            </Fade>
+          </div>
+
           <Fade right>
             <div>
               <img
@@ -146,7 +244,7 @@ export default function About() {
             </div>
           </Fade>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
