80f9a1f2630ceadbc4b57cece143e6d2b593e9c1
Aamir Alam <aamiralam1991@gmail.com>
2021-05-12 12:54:56 +0530

    Add header page

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -1,87 +1,73 @@
-import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
-import { CallToAction, ShutterSpeed } from '@material-ui/icons';
-import Pulse from 'react-reveal/Pulse';
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Pulse from "react-reveal/Pulse";
+import CustomButton from "../../common/CustomButton";
 
 const useStyles = makeStyles((theme) => ({
-  spacing: {
-    paddingTop: 130,
-
-    [theme.breakpoints.down('sm')]: {
-      paddingTop: 50,
-      textAlign: 'center',
-    },
-  },
-  circle: {
-    width: 7,
-    height: 7,
-    padding: 5,
-    margin: 6,
-    borderRadius: '50%',
-    backgroundColor: theme.palette.pbr.primary,
-  },
   header: {
-    height: '80vh',
-    width: 'auto',
+    height: "100vh",
+    width: "auto",
     background: `linear-gradient(0deg, rgba(255, 255, 255, 0.8), rgba(233, 233, 233, 1) ),url("https://img.freepik.com/free-vector/variety-cute-shapes-abstract-background_23-2148544989.jpg?size=626&ext=jpg&ga=GA1.2.1991903213.1616889600")`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    [theme.breakpoints.up('sm')]: {
-      height: '85vh',
-    },
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
   },
   notice: {
     fontWeight: 400,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
     margin: 0,
     paddingBottom: 15,
+    color: theme.palette.pbr.primary,
   },
   heading: {
     color: theme.palette.pbr.textSecondary,
-    width: 'auto',
-    textAlign: 'center',
-    fontSize: '52px',
+    width: "auto",
+    textAlign: "center",
+    fontSize: "52px",
 
     fontWeight: 600,
-    verticalAlign: 'middle',
-    wordSpacing: '0px',
-    margin: '0px 0px 12px',
-    padding: '0px 12px',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '32px',
+    verticalAlign: "middle",
+    wordSpacing: "0px",
+    margin: "0px 0px 12px",
+    padding: "0px 12px",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "32px",
+    },
+  },
+  headerContainer: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // flexWrap: "wrap",
+    paddingTop: 100,
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column-reverse",
+    },
+  },
+  imageContainer: {
+    width: 550,
+    height: "auto",
+
+    [theme.breakpoints.down("xs")]: {
+      width: 300,
+      height: "auto",
+    },
+  },
+  textContainer: {
+    maxWidth: 600,
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.down("md")]: {
+      alignItems: "center",
     },
   },
   subheading: {
-    textAlign: 'center',
-    paddingTop: 20,
-    fontSize: 16,
+    paddingTop: 15,
+    paddingBottom: 15,
+    fontSize: 18,
     fontWeight: 400,
     fontColor: theme.palette.pbr.textLight,
-    marginTop: 25,
-  },
-  button: {
-    borderRadius: '50px',
-    background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    lineHeight: '24px',
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
-    margin: 0,
-    marginTop: 15,
-    color: '#ffffff',
-    padding: '15px 30px 15px 30px',
-    fontWeight: 600,
-    fontSize: 14,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 13,
-      padding: '10px 20px 10px 20px',
-    },
-  },
-  icon: {
-    fontSize: 16,
-    marginRight: 7,
-    color: '#ffffff',
+    // marginTop: 25,
   },
 }));
 
@@ -90,31 +76,23 @@ export default function Header() {
   return (
     <Fragment>
       <section>
-        {' '}
         <div className={classes.header}>
-          <div className={classes.spacing}>
-            <div className="d-flex flex-row justify-content-center mt-5"></div>
+          <div className={classes.headerContainer}>
             <Pulse>
-              <h1 className={classes.heading}>
-                CORGI - A community-focused <br />
-                decentralized transaction network
-              </h1>
+              <img
+                src="images/sanshu_party.png"
+                alt=""
+                className={classes.imageContainer}
+              />
             </Pulse>
-
-            <div className="text-center mt-3">
-              <a href="https://farm.polkabridge.org/" style={{ textDecoration: 'none', padding: 5 }}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  <ShutterSpeed className={classes.icon} />
-                  PancakeSwap
-                </Button>
-              </a>
-              <a
-                href="https://docs.google.com/forms/d/1ceBZIL8xDNBJWYzZ4j11lhU9MMke8usrNgnPEoSIMf0"
-                style={{ textDecoration: 'none', padding: 5 }}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  <CallToAction className={classes.icon} /> Buy PBR
-                </Button>
-              </a>
+            <div className={classes.textContainer}>
+              <h5 className={classes.notice}>CORGIB Finance</h5>
+              <h2>The NFT MarketPlace for Meme coins</h2>
+              <p className={classes.subheading}>
+                CORGIB is fully decentralized, and all decisions are made by the
+                community.
+              </p>
+              <CustomButton title={"Buy on uniswap"}></CustomButton>
             </div>
           </div>
         </div>
