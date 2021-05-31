88b4ca95300e7fbcb26d57ab310f2b7aaa36fa58
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-05-31 21:33:15 +0530

    circulating supply api

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import Home from './pages/Home/Home';
 import './App.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { theme } from './theme';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Supply from './components/Supply';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Fragment>
-        <Appbar />
-        <Home />
-      </Fragment>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/">
+            <Fragment>
+              <Appbar />
+              <Home />
+            </Fragment>
+          </Route>
+          <Route exact path="/api/cirsupply">
+            <Fragment>
+              <Supply />
+            </Fragment>
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
